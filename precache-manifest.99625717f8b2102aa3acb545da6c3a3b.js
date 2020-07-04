bde8a10f19291ad6c4edbfc60b2b2ee90ad037c4
Mahmoud Abdo <mahmoud.abdo59986@gmail.com>
2020-07-04 11:51:25 +0200

    Updates

---
+++ precache-manifest.99625717f8b2102aa3acb545da6c3a3b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1ba3f2f247be46b1763dc0a2054017",
+    "url": "/Mahmoud-Abdo/index.html"
+  },
+  {
+    "revision": "64ca2f81edad4e024426",
+    "url": "/Mahmoud-Abdo/static/css/main.844bc39d.chunk.css"
+  },
+  {
+    "revision": "f8a097be502d64246195",
+    "url": "/Mahmoud-Abdo/static/js/2.28125f18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mahmoud-Abdo/static/js/2.28125f18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64ca2f81edad4e024426",
+    "url": "/Mahmoud-Abdo/static/js/main.775d19e7.chunk.js"
+  },
+  {
+    "revision": "6b7013f34e0b68946455",
+    "url": "/Mahmoud-Abdo/static/js/runtime-main.f6fca8be.js"
+  },
+  {
+    "revision": "7aee3ac509dd8cde80f497f28cc4d451",
+    "url": "/Mahmoud-Abdo/static/media/about-2.7aee3ac5.jpg"
+  },
+  {
+    "revision": "dbd9a5f415fc4ad1e3cf94381321cbe4",
+    "url": "/Mahmoud-Abdo/static/media/cover-1.dbd9a5f4.jpg"
+  },
+  {
+    "revision": "09fa4639ef363d00cffdee22d04bbc47",
+    "url": "/Mahmoud-Abdo/static/media/cover-2.09fa4639.jpg"
+  },
+  {
+    "revision": "fce5a99f9f4a76410c3b4abda7889c78",
+    "url": "/Mahmoud-Abdo/static/media/e-commerce.fce5a99f.jpg"
+  },
+  {
+    "revision": "f41ff2b52046fd85339b520712c35f5f",
+    "url": "/Mahmoud-Abdo/static/media/guest-book.f41ff2b5.jpg"
+  },
+  {
+    "revision": "177764eadf43efc393ebb98329a774b2",
+    "url": "/Mahmoud-Abdo/static/media/linea-basic-10.177764ea.svg"
+  },
+  {
+    "revision": "61f3a8a387a64abfc0535cd0813cd71a",
+    "url": "/Mahmoud-Abdo/static/media/linea-basic-10.61f3a8a3.woff"
+  },
+  {
+    "revision": "e4de528c00da0c081906c3caea289f0f",
+    "url": "/Mahmoud-Abdo/static/media/linea-basic-10.e4de528c.eot"
+  },
+  {
+    "revision": "e6763f008205c55db623cdf68bbd581f",
+    "url": "/Mahmoud-Abdo/static/media/linea-basic-10.e6763f00.ttf"
+  },
+  {
+    "revision": "774139214139db24cfb582f19342f04c",
+    "url": "/Mahmoud-Abdo/static/media/lyric.77413921.jpg"
+  },
+  {
+    "revision": "7eee697a5835b98e589c7d55c1a44692",
+    "url": "/Mahmoud-Abdo/static/media/natures.7eee697a.jpg"
+  },
+  {
+    "revision": "825da96ee53afc8821d53d8a20443d84",
+    "url": "/Mahmoud-Abdo/static/media/to-do.825da96e.jpg"
+  },
+  {
+    "revision": "4ece748fbf3f6e1c940e7f9aebb406c8",
+    "url": "/Mahmoud-Abdo/static/media/weather.4ece748f.jpg"
+  }
+]);
